6c426f3897efe5b76735f37b7a36264a9e121d14
xxie-xd <xxie_xd@163.com>
2024-08-26 19:23:59 +0800

    Enable huge page support

--- conf/post-build.sh
+++ conf/post-build.sh
@@ -4,3 +4,5 @@ BR_ROOT=$PWD
 # setup mount for 9p FS tag /dev/boot
 echo '/dev/root /root/spec 9p rw,relatime,access=client,msize=8192,trans=virtio 0 0' >> $TARGETDIR/etc/fstab
 
+# setup Huge page supports
+echo 'vm.nr_hugepages = 512' >> $TARGETDIR/etc/sysctl.conf
